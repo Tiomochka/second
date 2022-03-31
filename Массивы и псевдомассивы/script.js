d2a5c93ac3c0ba515069bd1bd4e9174671dee408
Artem Plakhotniuk <artyom.plakhotnyuk@gmail.com>
2022-03-31 19:06:42 +0300

    массивы

---
+++ Массивы и псевдомассивы/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const arr = [16, 2, 33, 6, 8];
+
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i =0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// } 
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри масиива ${arr}`);
+// });
+
+const str = prompt("", "");
+const products = str.split(", ");
+console.log(products);
+// products.sort();
+// console.log(products.join(';'));

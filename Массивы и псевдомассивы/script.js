d2bf5d5bf917b2b31b42a7a825cc43cc06a2f118
Artem Plakhotniuk <artyom.plakhotnyuk@gmail.com>
2022-04-03 16:39:37 +0300

    crypta

--- Массивы и псевдомассивы/script.js
+++ Массивы и псевдомассивы/script.js
@@ -2,35 +2,35 @@
 
 const arr = [16, 2, 33, 6, 8];
 
-// arr.sort(compareNum);
-// console.log(arr);
+arr.sort(compareNum);
+console.log(arr);
 
-// function compareNum(a, b) {
-//     return a - b;
-// }
+function compareNum(a, b) {
+    return a - b;
+}
 
-// arr.pop();
-// arr.push(10);
+arr.pop();
+arr.push(10);
 
-// console.log(arr);
+console.log(arr);
 
-// for (let i =0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+for (let i =0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
 
-// for (let value of arr) {
-//     console.log(value);
-// } 
-// arr[99] = 0;
-// console.log(arr.length);
-// console.log(arr);
+for (let value of arr) {
+    console.log(value);
+} 
+arr[99] = 0;
+console.log(arr.length);
+console.log(arr);
 
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} внутри масиива ${arr}`);
-// });
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри масиива ${arr}`);
+});
 
 const str = prompt("", "");
 const products = str.split(", ");
 console.log(products);
-// products.sort();
-// console.log(products.join(';'));
+products.sort();
+console.log(products.join(';'));
